9a84b6d58708fd9a163eee19993b26b5b8112ea6
atharvaavhad <69342468+atharvaavhad@users.noreply.github.com>
2020-08-25 11:01:53 +0530

    ddddddddddddddddddddddddddddddddddddd

--- preload.js
+++ preload.js
@@ -6,7 +6,12 @@ window.addEventListener('DOMContentLoaded', () => {
     if (element) element.innerText = text
   }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
-})
+  for (const type of ['chrome', 'node', 'electron']) {dddddddddddddddddddddddddddddddddddddddddddddddddddd
+dddddddddddddddddddddddddddddddddddddd
+
+
+d
+f
+sd
+fad
+fadfas8das4dfa9sdf8sz4dfa4 5sd9xzdf8z9808sd04f8a49 84s 89f4s098f04s8d4f 89d4f89sff8sdf 8s4f7s404sdcv s 
